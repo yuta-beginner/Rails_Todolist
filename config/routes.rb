7e65fb7839e339b00f762f34eb1a686f7daf4937
yuta_begginer <sy03141995139@yahoo.co.jp>
2021-08-31 01:07:22 +0900

    finish 2-6

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  resources :todos
-  resources :goals
+  resources :goals do
+    resources :todos do
+      member do
+        get "sort"
+      end
+    end
+  end
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   root 'home#index'
 end
